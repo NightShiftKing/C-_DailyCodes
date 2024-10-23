d98187529f53024c401f346960a7ab53eeb2def9
NightShiftKing <98564476+NightShiftKing@users.noreply.github.com>
2024-10-23 15:17:25 -0600

    Add files via upload

--- c++DailyCodes/Functions/c++Functions.cpp
+++ c++DailyCodes/Functions/c++Functions.cpp
@@ -3,17 +3,54 @@
 using namespace std;
 
 
-int sumDigits(int n){
- char input[225] {};
- cout << "input a number" << endl;
- cin.getline(input, size(input));
-    
+int sumDigits(int num); 
+
+double area(double length, double width);
+
+bool isPrime(int num); 
+
+bool isPrime(int num){
+    if(num <= 1)
+        return false; 
+
+}
 
 
+double area(double length, double width){
+    double product = length * width; 
+    return product; 
+}
 
+int sumDigits(int num){
+ int digit = 0;
+
+ while(num != 0){
+    digit += num % 10; 
+    num /=10;
+ }
+ return digit; 
 }
 
 int main(){
+    int number;
 
+    cout << "Enter a number" << endl; 
+    cin >> number; 
+
+    cout << "sum of digits is " << endl;
+    cout << sumDigits(number); 
+
+    double resposeLength;
+    double responseWidth;
+
+    cout << "enter a length" << endl;  
+    cin >> resposeLength;
+
+    cout << "enter a width";
+    cin >> responseWidth;
+
+    cout << "The total area is " << endl;
+    cout << area(responseWidth, resposeLength); 
 
 }
+
--- c++DailyCodes/quiz.cpp
+++ c++DailyCodes/quiz.cpp
@@ -23,42 +23,19 @@ struct Base {
       
 };
 
-
+void printQ(vector<SubBase> question);
 
 int main() {
-    // Create a vector to hold Starfinder classes
-    vector<Base> Ancesteries = {
-        {"Android", {{"artisan", 0}, { "laborer", 0}, { "polyglot", 0}, { "warrior",0 }, { "scion", 0}, { "mod", 0}, { "networked",0 }, {"renewed", 0}}},
-        {"Barathu", {{"dreamer", 0}, { "earlystage", 0}, { "manufacturer", 0}, {"merged", 0}}},
-        {"Human", {{"azlanti",0}, {"golarion",0}, {"scavenger",0}, {"skilled",0}, {"urbanite",0}, {"versatile", 0}}},
-        {"Kasatha", {{"akitonian",0}, {"survivalist",0}, {"shipborn",0}, {"nomad",0}, {"tempreing",0}}},
-        {"Lashunta", {{"damaya",0}, {"korasha",0}, {"unbound",0}}},
-        {"Pahtra", {{"hunter",0}, {"krreyvash",0}, {"chosen",0}, {"pouncer",0}, {"sand",0}, {"rime",0}}},
-        {"Shirren", {{"courtier",0}, {"defiant",0}, {"infiltrator",0}, {"swarm",0}, {"winged",0}}},
-        {"Skittermander", {{"everwhelp",0}, {"gadraveech",0}, {"scrabbler",0}, {"skitter",0}}},
-        {"Shirren", {{"courtier",0}, {"defiant",0}, {"infiltrator",0}, {"swarm",0}, {"winged",0}}},
-        {"Vesk", {{"briskwander",0}, {"nightstalker",0}, {"plated",0}, {"venomthought",0}, {"warblood",0}}},
-        {"Ysoki", {{"akitonian",0}, {"deep",0}, {"longsnout",0}, {"shipborn",0}, {"tunnel",0}}}
-    };
 
-vector<Base> Classes = {
-	{"Operative", {{"ghost",0}, {"infiltrator",0}, {"skirmisher",0}, {"sniper",0}, {"striker",0}}},
-	{"Mystic", {{"akashic",0}, {"elemental",0}, {"healing",0}, {"rhythm",0}, {"shadow",0}}},
-	{"Soldier", {{"action hero",0}, {"armor storm",0}, {"Bombard",0}, {"close quarters",0}, {"erudite warrior",0}}}
-
-};
-
-
-    vector<SubBase> Backgrounds = {
+    vector<SubBase> question = {
         {"cleaner",0}, {"athlete",0}, {"comedian",0}, {"brutaris player",0}, {"corporate agent",0}, {"city slicker",0},
-        {"cyberborn",0}, {"diplomat",0}, {"dream prophet",0}, {"disciple",0}, {"electrician",0}, {"doctor",0}, {"gene splicer",0},
-        {"recluse",0}, {"grifter",0}, {"scientist",0}, {"hacker",0}, {"socialite",0}, {"icon",0}, {"smuggler",0}, {"outlaw",0},
-        {"spacefarer",0}, {"space pirate",0}, {"street rat",0}, {"tech support",0}, {"trooper",0}, {"vidgamer",0}
     };
 
-    Ancesteries[0].subclasses[0].points += 5; 
+    printQ()
 
-    cout << Ancesteries[0].subclasses[0].points << endl; 
 }
-
+   
+void printQ(vector<SubBase> question){
+        cout<< question[0].name << endl; 
+    }
 
